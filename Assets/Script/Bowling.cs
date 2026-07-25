da321ca84d768eb84a2ecace7121f015641a378d
valentia26 <Matepiya.n@gmail.com>
2026-07-25 10:32:08 +0700

    Bowlingphysich

---
+++ Assets/Script/Bowling.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Bowling : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rb;
+
+    [SerializeField]
+    private int forcePower;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
