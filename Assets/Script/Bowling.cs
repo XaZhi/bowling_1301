366dc5901f827f858ea5afd1cbb938c844bec9a0
valentia26 <Matepiya.n@gmail.com>
2026-07-25 10:47:51 +0700

    FirstGamePlay

--- Assets/Script/Bowling.cs
+++ Assets/Script/Bowling.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Bowling : MonoBehaviour
 {
@@ -11,12 +12,19 @@ public class Bowling : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            ShootBall();
+    }
+
+    private void ShootBall()
+    {
+        rb.AddForce(Vector3.forward * forcePower, ForceMode.Impulse);
     }
 }
